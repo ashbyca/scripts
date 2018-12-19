e397b8865aa33d39ebf4ad30260ebea6ecfc545c
Christopher Ashby <ashbyca@users.noreply.github.com>
2018-12-19 11:01:50 -0500

    Add files via upload

---
+++ dnslookup.bat
@@ -0,0 +1,19 @@
+@echo off
+if exist <dir>nsresults.txt del nsresults.txt
+
+:: list of name servers
+for %%i in (ns1
+ns2) do (
+
+:: list of sites
+for %%j in (endpoint1
+endpoint2) do (
+
+for /f "tokens=1*" %%k in ('nslookup %%j %%i') do (
+if [%%k]==[Server:] set server=%%l
+if [%%k]==[Address:] set address=%%l
+if [%%k]==[Addresses:] set address=%%l
+if [%%k]==[Name:] set name=%%l)
+call echo %date%, %time%, %%j, %%name%%, %%address%% >><dir>nsresults.txt
+)
+) 
