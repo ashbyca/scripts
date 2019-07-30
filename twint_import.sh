f6ccd9f489d685e3be08790f19bd558637b647a6
Christopher Ashby <ashbyca@users.noreply.github.com>
2019-07-30 11:08:49 -0400

    Update twint_import.sh
    updated script to include path

--- twint_import.sh
+++ twint_import.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
-  
+
 #Set Date Variable
 #day=$(date -d yesterday +"%Y-%m-%d")
 day=$(date --date '-60 min' +"%Y-%m-%d")
 
 # Ransomeware Attacks
-twint --elasticsearch localhost:9200 -s "ransomware OR decryptors" --since $day --limit 3200
-sleep 1m
-twint --elasticsearch localhost:9200 -s "malware OR phishing" --since $day --limit 3200
-sleep 1m
-
-echo "Completed importing ransomware tweets, queuing up active attacks..."
+~/.local/bin/twint --elasticsearch localhost:9200 -s "ransomware OR decryptors" --since $day --limit 3200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "malware OR phishing" --since $day --limit 3200
 
 # Active Attacks
-twint --elasticsearch localhost:9200 -s "cyberattack OR password" --since $day --limit 3200
-sleep 1m
-twint --elasticsearch localhost:9200 -s "exploit OR breach" --since $day --limit 3200
-sleep 1m
-twint --elasticsearch localhost:9200 -s "cybercrime OR blackout OR databreach" --since $day --limit 3200
-sleep 1m
-
-echo "Completed importing active attack tweets, queuing up industry events..."
+~/.local/bin/twint --elasticsearch localhost:9200 -s "cyberattack OR password" --since $day --limit 3200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "exploit OR breach" --since $day --limit 3200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "cybercrime OR blackout OR databreach" --since $day --limit 3200
 
 #Industry Topics
-twint --elasticsearch localhost:9200 -s "infosec OR IOTSecurity OR Securitybydesign" --since $day --limit 3200
-sleep 1m
-twint --elasticsearch localhost:9200 -s "cybersecurity OR threatintelligence OR osint OR dataprivacy" --since $day --limit 3200
-sleep 1m
-
-echo "Completed importing industry events, all infromation has been imported! Cleaning up..."
+~/.local/bin/twint --elasticsearch localhost:9200 -s "infosec OR IOTSecurity OR Securitybydesign" --since $day --limit 3200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "cybersecurity OR threatintelligence OR osint OR dataprivacy" --since $day --limit 3200
