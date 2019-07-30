fa1b76795607ef8dfc9accc89744b347649b24a9
Christopher Ashby <ashbyca@users.noreply.github.com>
2019-07-30 12:40:47 -0400

    Update twint_import.sh
    Added additional accounts

--- twint_import.sh
+++ twint_import.sh
@@ -10,6 +10,18 @@ day=$(date --date '-60 min' +"%Y-%m-%d")
 ~/.local/bin/twint --elasticsearch localhost:9200 -s "USCERT_gov OR neiljrubenking OR dangoodin001 OR campuscodi OR peterkruse OR shirastweet OR nakashimae OR iblametom OR evacide OR DanielMiessler" --since $day --limit 200
 ~/.local/bin/twint --elasticsearch localhost:9200 -s "evanderburg OR ScottBVS OR jack_daniel OR anton_chuvakin OR lennyzeltser OR JosephSteinberg OR RobertMLee OR TroelsOerting OR ejhilbert OR PatrickCMiller" --since $day --limit 200
 
+# Cyber Publications
+~/.local/bin/twint --elasticsearch localhost:9200 -s "InfosecurityMag OR TripwireInc OR securityaffairs OR eWEEKNews OR threatpost" --since $day --limit 200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "thehackersnews OR HackRead OR BetaNews OR ghacksnews OR itwire" --since $day --limit 200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "CSOonline OR CIOonline OR SecurityWeek OR securityweekly OR DarkReading OR helpnetsecurity OR scmagazine" --since $day --limit 200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "motherboard OR TheRegister OR fedscoop OR BleepinComputer OR networkworld OR computing_news" --since $day --limit 200
+
+# Cyber Companies
+~/.local/bin/twint --elasticsearch localhost:9200 -s "Bitdefender OR Malwarebytes OR Onion_ID OR moixsec OR uscert_gov" --since $day --limit 200
+~/.local/bin/twint --elasticsearch localhost:9200 -s "NakedSecurity OR kaspersky OR FSecure OR NortonOnline OR ncsc" --since $day --limit 200
+
+# Interesting
+~/.local/bin/twint --elasticsearch localhost:9200 -s "Peerlyst OR cyber OR FieldfisherPSI OR malware_traffic OR privacyfocused" --since $day --limit 200
 
 # Ransomeware Attacks
 ~/.local/bin/twint --elasticsearch localhost:9200 -s "ransomware OR decryptors" --since $day --limit 3200
