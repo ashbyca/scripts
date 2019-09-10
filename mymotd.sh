c8f0a8665b3ea164af15a60f73f9a489528ea1d9
Christopher Ashby <ashbyca@users.noreply.github.com>
2019-09-10 11:15:30 -0400

    Update mymotd.sh

--- mymotd.sh
+++ mymotd.sh
@@ -9,13 +9,22 @@ sudo apt-get install lsb-release && sudo apt-get install figlet && sudo apt-get 
 # Remove the current directory
 sudo rm -r /etc/update-motd.d/
 
-# Create new directory and download dynamic files
+# Create new directory
 sudo mkdir /etc/update-motd.d/
-Wwget
-# create dynamic files
-touch /etc/update-motd.d/00-header ; touch /etc/update-motd.d/10-sysinfo ; touch /etc/update-motd.d/90-footer
 
+# Download 00-header, 10-sysinfo, 90-footer and change permissions
+cd /etc/update-motd.d/ 
 
-chmod +x /etc/update-motd.d/*
+sudo wget https://gist.github.com/ashbyca/9ce85acbcb17e5ecd6055e9c88837158
+sudo mv 9ce85acbcb17e5ecd6055e9c88837158 00-header
+
+sudo wget https://gist.github.com/ashbyca/582cf06398ceeb651548bef1743605b8
+sudo mv 582cf06398ceeb651548bef1743605b8 10-sysinfo
+
+sudo wget https://gist.github.com/ashbyca/a7bd031c7efdd9c9c5fe53f9a5dc49ae
+sudo mv a7bd031c7efdd9c9c5fe53f9a5dc49ae 90-footer
+
+sudo chmod +x /etc/update-motd.d/*
+
 # remove MOTD file
 rm /etc/motd.dynamic
