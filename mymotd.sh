b0398b57e1c4bf5e4a0ff103a836f8d7ed39a3e2
Christopher Ashby <ashbyca@users.noreply.github.com>
2019-09-10 11:29:16 -0400

    Update mymotd.sh

--- mymotd.sh
+++ mymotd.sh
@@ -16,13 +16,13 @@ sudo mkdir /etc/update-motd.d/
 cd /etc/update-motd.d/ 
 
 sudo wget https://gist.githubusercontent.com/ashbyca/9ce85acbcb17e5ecd6055e9c88837158/raw/a0d9dcfd5ff4283ab8cdcdbeedb24796dfd4946b/00-header
-sudo mv 9ce85acbcb17e5ecd6055e9c88837158 00-header
+#sudo mv 9ce85acbcb17e5ecd6055e9c88837158 00-header
 
 sudo wget https://gist.githubusercontent.com/ashbyca/582cf06398ceeb651548bef1743605b8/raw/6785d9f3c08105959c69d285560dc76076ae649e/10-sysinfo
-sudo mv 582cf06398ceeb651548bef1743605b8 10-sysinfo
+#sudo mv 582cf06398ceeb651548bef1743605b8 10-sysinfo
 
 sudo wget https://gist.githubusercontent.com/ashbyca/a7bd031c7efdd9c9c5fe53f9a5dc49ae/raw/20be9196dfd3fbceac2e074d6b3dd0956892ac6f/90-footer
-sudo mv a7bd031c7efdd9c9c5fe53f9a5dc49ae 90-footer
+#sudo mv a7bd031c7efdd9c9c5fe53f9a5dc49ae 90-footer
 
 sudo chmod +x /etc/update-motd.d/*
 
